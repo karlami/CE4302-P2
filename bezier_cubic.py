463cda3b4d446edf4a4539ad6416b91f69c2ad4b
Karla Rivera <karla.rivera24@gmail.com>
2023-10-22 22:39:29 -0600

    bezier fixed

--- bezier_cubic.py
+++ bezier_cubic.py
@@ -7,10 +7,11 @@ def bezier_cubic(p0, p1, p2, p3, x_values, y_values):
     for i in range(0, 101, 2):  # Usamos 101 para incluir el valor final 1.0
         t = i / 100.0  # Calculamos t en el rango de 0 a 1
         t1 = (i + 1) / 100.0  # Calculamos el siguiente valor de t
-        x = (1 - t)*3 * p0[0] + 3 * (1 - t)2 * t * p1[0] + 3 * (1 - t) * t2 * p2[0] + t*3 * p3[0]
-        y = (1 - t)*3 * p0[1] + 3 * (1 - t)2 * t * p1[1] + 3 * (1 - t) * t2 * p2[1] + t*3 * p3[1]
-        x1 = (1 - t1)*3 * p0[0] + 3 * (1 - t1)2 * t1 * p1[0] + 3 * (1 - t1) * t12 * p2[0] + t1*3 * p3[0]
-        y1 = (1 - t1)*3 * p0[1] + 3 * (1 - t1)2 * t1 * p1[1] + 3 * (1 - t1) * t12 * p2[1] + t1*3 * p3[1]
+        # Loop unrolling de profundidad 2
+        x = (1 - t)**3 * p0[0] + 3 * (1 - t)**2 * t * p1[0] + 3 * (1 - t) * t**2 * p2[0] + t**3 * p3[0]
+        y = (1 - t)**3 * p0[1] + 3 * (1 - t)**2 * t * p1[1] + 3 * (1 - t) * t**2 * p2[1] + t**3 * p3[1]
+        x1 = (1 - t1)**3 * p0[0] + 3 * (1 - t1)**2 * t1 * p1[0] + 3 * (1 - t1) * t1**2 * p2[0] + t1**3 * p3[0]
+        y1 = (1 - t1)**3 * p0[1] + 3 * (1 - t1)**2 * t1 * p1[1] + 3 * (1 - t1) * t1**2 * p2[1] + t1**3 * p3[1]
         x_values.append(x)
         y_values.append(y)
         x_values.append(x1)
@@ -23,13 +24,13 @@ width, height = 640, 480
 image = np.zeros((height, width, 3), dtype=np.uint8)
 
 # Puntos de control necesarios para crear la curva
+# No pueden sobrepasar los límites de la imagen
 p0 = (50, 400)
 p1 = (200, 100)
 p2 = (400, 100)
 p3 = (550, 400)
 
-# Parámetros de t (0 a 1)
-t_values = np.linspace(0, 1, 100)
+# Parámetros de x y y para la curva Bezier
 x_values = []
 y_values = []
 
--- t.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Generar una secuencia de valores de t
-t_values = np.linspace(0, 1, 100)
-
-# Imprimir la secuencia de valores de t
-print(t_values)
