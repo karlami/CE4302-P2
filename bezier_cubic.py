08b78f2489b16e123e5e9bca43746c10a09c021e
Karla Rivera <karla.rivera24@gmail.com>
2023-10-22 19:34:54 -0600

    loop

--- bezier_cubic.py
+++ bezier_cubic.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-# Calcular un punto en la curva Bezier cubica
-def bezier_cubic(t, p0, p1, p2, p3):
-    x = (1 - t)**3 * p0[0] + 3 * (1 - t)**2 * t * p1[0] + 3 * (1 - t) * t**2 * p2[0] + t**3 * p3[0]
-    y = (1 - t)**3 * p0[1] + 3 * (1 - t)**2 * t * p1[1] + 3 * (1 - t) * t**2 * p2[1] + t**3 * p3[1]
-    return x, y
+# Función para calcular un punto en la curva Bézier cúbica y guardar los valores
+def bezier_cubic(p0, p1, p2, p3, x_values, y_values):
+    for t in range(0, 100, 2(1/99)):
+        x = (1 - t)**3 * p0[0] + 3 * (1 - t)**2 * t * p1[0] + 3 * (1 - t) * t**2 * p2[0] + t**3 * p3[0]
+        y = (1 - t)**3 * p0[1] + 3 * (1 - t)**2 * t * p1[1] + 3 * (1 - t) * t**2 * p2[1] + t**3 * p3[1]
+        x1 = (1 - (t+1/99))**3 * p0[0] + 3 * (1 - (t+1/99))**2 * (t+1/99) * p1[0] + 3 * (1 - (t+1/99)) * (t+1/99)**2 * p2[0] + (t+1/99)**3 * p3[0]
+        y1 = (1 - (t+1/99))**3 * p0[1] + 3 * (1 - (t+1/99))**2 * (t+1/99) * p1[1] + 3 * (1 - (t+1/99)) * (t+1/99)**2 * p2[1] + (t+1/99)**3 * p3[1]
+    x_values.append(x)
+    y_values.append(y)
+    x_values.append(x1)
+    y_values.append(y1)
 
 # Dimensiones de la imagen
 width, height = 640, 480
@@ -22,13 +28,12 @@ p3 = (550, 400)
 
 # Parámetros de t (0 a 1)
 t_values = np.linspace(0, 1, 100)
-
-# Calcular las coordenadas (x, y) en la curva Bezier cubica
-curve_points = [bezier_cubic(t, p0, p1, p2, p3) for t in t_values]
+x_values = []
+y_values = []
 
 # Dibujar la curva Bezier en la imagen
-for x, y in curve_points:
-    image[int(y), int(x)] = (255, 255, 255)  # Pixel blanco en la ubicacion de la curva
+for x, y in zip(x_values, y_values):
+    image[int(y), int(x)] = (255, 255, 255)
 
 # Mostrar la imagen con la curva Bezier
 cv2.imshow('Bezier Curve', image)
---
+++ t.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Generar una secuencia de valores de t
+t_values = np.linspace(0, 1, 100)
+
+# Imprimir la secuencia de valores de t
+print(t_values)
